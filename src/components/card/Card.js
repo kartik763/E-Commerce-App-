7864bb9f2fe761de6ac5b782c45532dcab711fc3
Mananx23 <manan.olympian23@gmail.com>
2020-10-20 19:10:33 +0530

    initial commit

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -9,7 +9,7 @@ const Card = ()=>(
   <div className="card-body">
     <h5 className="card-title">NIKE</h5>
     <p className="card-text">$9.99</p>
-    <a href="#" className="btn btn-primary">Add to bag</a>
+    <a href="#" className="btn btn-primary">Add to Cart</a>
   </div>
 </div>
 );
